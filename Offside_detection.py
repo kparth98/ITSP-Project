e6aa4634d5ad5ce44a06fd0d3f45dbe03331c5fb
yvsriram <yvsriram99@gmail.com>
2017-07-12 17:23:01 +0530

    Resolved merge conflict

--- Offside_detection.py
+++ Offside_detection.py
@@ -8,21 +8,20 @@ frame = None
 orig_frame = None
 roi_hist_A, roi_hist_B = None, None
 roi = None
-<<<<<<< HEAD
+
 team = None
-=======
->>>>>>> b8a0bc709908e05faa88e3a204c624431dda394d
+
+
 teamA = np.array([])
 teamB = np.array([])
 teamB_new = np.array([])
 teamA_new = np.array([])
 pts = []
-<<<<<<< HEAD
+
 minDist = 0
 prevTeam = None
 prevPasser = -1
-=======
->>>>>>> b8a0bc709908e05faa88e3a204c624431dda394d
+
 M = None
 op = None
 limits = None
@@ -37,16 +36,14 @@ kernel = np.array([[0, 0, 1, 1, 0, 0],
 
 prevgrad = 0
 passes = 0
-<<<<<<< HEAD
+
 vel, prev_vel = 0, 0
-=======
-vel, prev_vel = 0,0
->>>>>>> b8a0bc709908e05faa88e3a204c624431dda394d
+
 pts_ball = deque()
 
 
 def trackBall():
-<<<<<<< HEAD
+
     global grad, prevgrad, passes, ball_center, pts_ball, frame, vel, prev_vel, prevPasser, prevTeam, minDist
     pts_ball.appendleft(ball_center)
 
@@ -58,23 +55,14 @@ def trackBall():
         else:
             for i in xrange(1, len(pts_ball)):
                 cv2.line(frame, pts_ball[i - 1], pts_ball[i], (0, 0, 255), 2, cv2.LINE_AA)
-=======
-    global grad, prevgrad, passes, ball_center, pts_ball, frame, vel, prev_vel
-    pts_ball.appendleft(ball_center)
 
-    '''if len(pts_ball) > 2:
-
-        for i in xrange(1, len(pts_ball)):
-            cv2.line(frame, pts_ball[i-1], pts_ball[i], (0, 0, 255), 2, cv2.LINE_AA)
-    '''
->>>>>>> b8a0bc709908e05faa88e3a204c624431dda394d
 
     l = len(pts_ball)
     if l >= 10:
         grad = np.arctan2((pts_ball[9][1] - pts_ball[0][1]), (pts_ball[9][0] - pts_ball[0][0]))
         grad = grad * (180.0 / np.pi)
         grad %= 360
-<<<<<<< HEAD
+
         vel = math.sqrt((pts_ball[9][1] - pts_ball[0][1]) ** 2 + (pts_ball[9][0] - pts_ball[0][0]) ** 2) / 10
         if (math.fabs(grad - prevgrad) >= 20):
             # or math.fabs(vel-prev_vel) >= 7:
@@ -100,21 +88,12 @@ def trackBall():
                 prevPasser = passerIndex
                 prevTeam = team
 
-=======
-        vel = math.sqrt((pts_ball[9][1] - pts_ball[0][1]) ** 2 + (pts_ball[9][0] - pts_ball[0][0]) ** 2)/10
-        if (math.fabs(grad - prevgrad) >= 20) :
-                #or math.fabs(vel-prev_vel) >= 7:
-            print('Ball Passed ' + str(passes))
-            passes += 1
->>>>>>> b8a0bc709908e05faa88e3a204c624431dda394d
+
         prevgrad = grad
         prev_vel = vel
 
 
-<<<<<<< HEAD
-=======
 
->>>>>>> b8a0bc709908e05faa88e3a204c624431dda394d
 def detectPlayers():
     global frame, roi_hist_A, roi_hist_B, teamA, teamB
     teamA = []
@@ -172,11 +151,9 @@ def detectPlayers():
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
                 M = cv2.moments(c[i])
                 center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-<<<<<<< HEAD
+
                 foot = (center[0], int(center[1] + h * 1.2))
-=======
-                foot = (center[0], int(center[1] + h * 1.5))
->>>>>>> b8a0bc709908e05faa88e3a204c624431dda394d
+
                 teamB.append(foot)
                 cv2.circle(frame, foot, 5, (0, 0, 255), -1)
 
@@ -230,7 +207,7 @@ def getBoundaryPoints():
 
 
 def getCoordinates():
-<<<<<<< HEAD
+
     global M, teamA, teamB, op, teamB_new, teamA_new, ball_new, ball_center
     teamB_new = np.array([])
     teamA_new = np.array([])
@@ -239,29 +216,21 @@ def getCoordinates():
         new = np.dot(M, [ball_center[0], ball_center[1], 1])
         ball_new = [new[0] / new[2], new[1] / new[2]]
         op = cv2.circle(op, (int(ball_new[0]), int(ball_new[1])), 3, (255, 0, 0), -1)
-=======
-    global M, teamA, teamB, op, teamB_new, teamA_new
-    teamB_new = np.array([])
-    teamA_new = np.array([])
->>>>>>> b8a0bc709908e05faa88e3a204c624431dda394d
+
     if len(teamB) > 0:
         for i in range(len(teamB)):
             new_pt = np.dot(M, [teamB[i][0], teamB[i][1], 1])
             teamB_new = np.append(teamB_new, [new_pt[0] / new_pt[2], new_pt[1] / new_pt[2]])
-<<<<<<< HEAD
+
         teamB_new = np.int16(teamB_new).reshape(-1, 2)
         for i in range(len(teamB)):
             op = cv2.circle(op, (teamB_new[i][0], teamB_new[i][1]), 5, (0, 255, 0), -1)
-=======
-        teamB_new = np.int16(teamB_new).reshape(-1,2)
-        for i in range(len(teamB)):
-            op = cv2.circle(orig_op.copy(), (teamB_new[i][0], teamB_new[i][1]), 5, (0, 255, 0), -1)
->>>>>>> b8a0bc709908e05faa88e3a204c624431dda394d
+
     if len(teamA) > 0:
         for i in range(len(teamA)):
             new_pt = np.dot(M, [teamA[i][0], teamA[i][1], 1])
             teamA_new = np.append(teamA_new, [new_pt[0] / new_pt[2], new_pt[1] / new_pt[2]])
-<<<<<<< HEAD
+
         teamA_new = np.int16(teamA_new).reshape(-1, 2)
         for i in range(len(teamA)):
             op = cv2.circle(op, (teamA_new[i][0], teamA_new[i][1]), 5, (0, 0, 255), -1)
@@ -326,21 +295,7 @@ def detectOffside():
             print('Not Offside')
     else:
         print('Not Offside')
-=======
-        teamA_new = np.int16(teamA_new).reshape(-1,2)
-        for i in range(len(teamA)):
-            op = cv2.circle(op, (teamA_new[i][0], teamA_new[i][1]), 5, (0, 0, 255), -1)
 
-def drawOffsideLine():
-    global M, teamB_new, op, frame
-    if len(teamB_new) >0:
-        M_inv = np.linalg.inv(M)
-        p1 = np.dot(M_inv, [teamB_new[0][0], 0, 1])
-        p2 = np.dot(M_inv, [teamB_new[0][0], op.shape[0]-1, 1])
-
-        pts = [(int(p1[0]/p1[2]), int(p1[1]/p1[2])),(int(p2[0]/p2[2]), int(p2[1]/p2[2]))]
-        frame = cv2.line(frame, pts[0],pts[1],(255,0,0),2)
->>>>>>> b8a0bc709908e05faa88e3a204c624431dda394d
 
 
 if __name__ == '__main__':
@@ -378,17 +333,12 @@ if __name__ == '__main__':
 
         if M is not None:
             src = np.int32(src)
-<<<<<<< HEAD
+
             # print src
             for i in range(4):
                 frame = cv2.circle(frame.copy(), (src[i][0], src[i][1]), 5, (255, 0, 255), -1)
             # frame = cv2.polylines(frame.copy(), src,True,(255,0,255),3)
-=======
-            #print src
-            for i in range(4):
-                frame = cv2.circle(frame.copy(), (src[i][0],src[i][1]), 5, (255, 0, 255), -1)
-            #frame = cv2.polylines(frame.copy(), src,True,(255,0,255),3)
->>>>>>> b8a0bc709908e05faa88e3a204c624431dda394d
+
             getCoordinates()
             drawOffsideLine()
 
